d52e0328325a2cf592cc684f7f9a10baccb7bef1
jogabi <rkql1109@naver.com>
2025-08-19 16:30:10 +0900

    수정2

--- src/main/java/Maven/Project/HelloController.java
+++ src/main/java/Maven/Project/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String home() {
-        return "서버 연결 성공! 🎉";
+        return "서버 연결 성공안녕!!!!! 🎉";
     }
 }
