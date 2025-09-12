be6999eddaf5092285d824fe73e7839e22253cd1
jogabi <rkql1109@naver.com>
2025-09-12 16:45:13 +0900

    cors에러수정

--- src/main/java/Maven/Project/DemoApplication.java
+++ src/main/java/Maven/Project/DemoApplication.java
@@ -2,9 +2,6 @@ package Maven.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;                        // ⬅️ 추가
-import org.springframework.web.servlet.config.annotation.CorsRegistry; // ⬅️ 추가
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer; // ⬅️ 추가
 
 @SpringBootApplication
 public class DemoApplication {
