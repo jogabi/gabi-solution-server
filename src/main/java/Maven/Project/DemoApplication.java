805b1a32aef18f7f74376108718d78164bbd4284
jogabi <rkql1109@naver.com>
2025-09-12 16:43:23 +0900

    cors에러수정

--- src/main/java/Maven/Project/DemoApplication.java
+++ src/main/java/Maven/Project/DemoApplication.java
@@ -13,22 +13,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	// ✅ 전역 CORS 설정 추가
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins(
-								"http://localhost:5173",     // 로컬 개발
-								"https://www.gabisolution.xyz", // 실제 배포 도메인
-								"https://gabisolution.xyz"      // www 없는 도메인도 허용
-						)
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.maxAge(3600);
-			}
-		};
-	}
+
 }
